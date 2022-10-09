7a47196d47d0d5f941e6e574335462957f04715d
hanifjvm <hanifjvm@gmail.com>
2022-10-09 07:17:56 +0700

    simulasi depends on

---
+++ src/main/java/com/hanifiamdev/spring/core/DependsOnConfiguration.java
@@ -0,0 +1,30 @@
+package com.hanifiamdev.spring.core;
+
+import com.hanifiamdev.spring.core.data.Bar;
+import com.hanifiamdev.spring.core.data.Foo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+@Slf4j
+@Configuration
+public class DependsOnConfiguration {
+
+    @Bean
+    @DependsOn({
+            "bar"
+    })
+    public Foo foo(){
+        log.info("Create new Foo");
+        return new Foo();
+    }
+
+    @Bean
+    public Bar bar(){
+        log.info("Create new Bar");
+        return new Bar();
+    }
+
+
+}
