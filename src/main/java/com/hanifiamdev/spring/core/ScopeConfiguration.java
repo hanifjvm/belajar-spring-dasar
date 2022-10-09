893ae30b38a5fa9b17f68ec72523843f9958d825
hanifjvm <hanifjvm@gmail.com>
2022-10-09 18:59:39 +0700

    membuat scope custome

--- src/main/java/com/hanifiamdev/spring/core/ScopeConfiguration.java
+++ src/main/java/com/hanifiamdev/spring/core/ScopeConfiguration.java
@@ -1,7 +1,10 @@
 package com.hanifiamdev.spring.core;
 
+import com.hanifiamdev.spring.core.data.Bar;
 import com.hanifiamdev.spring.core.data.Foo;
+import com.hanifiamdev.spring.core.scope.DoubletonScope;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -16,5 +19,24 @@ public class ScopeConfiguration {
         log.info("Create new Foo");
         return new Foo();
     }
+
+    @Bean
+    public CustomScopeConfigurer customScopeConfigurer(){
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+        configurer.addScope("doubleton", new DoubletonScope());
+        return configurer;
+    }
+
+    @Bean
+    @Scope("doubleton")
+    public Bar bar(){
+        log.info("Create new Bar");
+        return new Bar();
+    }
+
+
+
+
+
 
 }
