cfe04ed2e00410e8a63b3d1f75418f964b122e0d
hanifjvm <hanifjvm@gmail.com>
2022-10-09 07:47:03 +0700

    simulasi lazy bean

--- src/main/java/com/hanifiamdev/spring/core/DependsOnConfiguration.java
+++ src/main/java/com/hanifiamdev/spring/core/DependsOnConfiguration.java
@@ -6,15 +6,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Lazy;
 
 @Slf4j
 @Configuration
 public class DependsOnConfiguration {
 
+    @Lazy
     @Bean
-    @DependsOn({
-            "bar"
-    })
+    @DependsOn(value={"bar"})
     public Foo foo(){
         log.info("Create new Foo");
         return new Foo();
--- src/test/java/com/hanifiamdev/spring/core/DependsOnTest.java
+++ src/test/java/com/hanifiamdev/spring/core/DependsOnTest.java
@@ -1,5 +1,6 @@
 package com.hanifiamdev.spring.core;
 
+import com.hanifiamdev.spring.core.data.Foo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
@@ -16,5 +17,8 @@ public class DependsOnTest {
 
     @Test
     void testDependsOn() {
+        // Lazy => bean akan dibuat setelah
+        Foo foo = applicationContext.getBean(Foo.class);
+
     }
 }
